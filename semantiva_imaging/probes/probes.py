9234cb378f6ce4fd81ae71b09cd3479026d890a9
Marcos Deros <marcosderos78@gmail.com>
2025-07-07 23:56:11 +0200

    [Issue 48] Private n channel operation probes (#17)

--- semantiva_imaging/probes/probes.py
+++ semantiva_imaging/probes/probes.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Dict
 from scipy.optimize import curve_fit
 import numpy as np
 from typing import Dict
